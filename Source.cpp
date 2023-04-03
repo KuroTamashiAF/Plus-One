75d0120bb092e8c19092ff32380db21cff84653f
Fedorov <chugun777akk@mail.ru>
2023-04-03 19:55:50 +0700

    basic tests passed

--- Source.cpp
+++ Source.cpp
@@ -2,25 +2,33 @@
 #include <vector>
 class Solution {
 public:
-    static std::vector<int> plusOne(std::vector<int>& digits) {
+    static std::vector<int> plusOne(std::vector<int>& digits) 
+    {
+        if (digits.empty())
+            return digits;
 
-
-
-
-
-
-
-
-
-
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 1;
+                digits.push_back(0);
+            }
+            else
+            {
+                digits[i] += 1;
+                break;
+            }
+        }
+        return digits;
     }
     static void print(std::vector<int>& digits) {
+        std::cout << '[';
         for (int i : digits)
         {
-            std::cout << '[';
             std::cout << i << ',';
-            std::cout << ']';
         }
+        std::cout << ']';
     }
 
 };
@@ -31,7 +39,7 @@ public:
 
 int main()
 {
-    std::vector<int> input = { 1,2,3 };
+    std::vector<int> input = { 9};
     Solution::plusOne(input);
     Solution::print(input);
 
